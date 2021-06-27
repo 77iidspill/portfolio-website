8140ea0bcfca0cc3130a32c4c736a77c5d9f4c66
77iidspill <85503634+77iidspill@users.noreply.github.com>
2021-06-27 18:07:35 -0400

    Header Edits and JS update

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-console.log("Hello and Welcome to My Portfolio Site!")
-function menuToggle () {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
     x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';  
-    }
-} 
+  } else {
+    x.className = 'navtoggle';
+  }
+}
